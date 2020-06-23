7cdb6d3b1cb6246afb3f7f45d5dffe20ee540bda
jgutierrez <orozcox@gmail.com>
2020-06-23 12:24:31 -0600

    docs agregado

---
+++ docs/precache-manifest.cfd4c0de70bc94cf4ee67a721c268257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1157058957c58ed1f2ad32de53c791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec314341c6aec576a03a",
+    "url": "/static/css/main.79dd7738.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec314341c6aec576a03a",
+    "url": "/static/js/main.6a2f1f25.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
